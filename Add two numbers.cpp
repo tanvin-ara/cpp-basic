13fd41e3158233439d0a5e2eab82de4ae91f5f18
tanvin-ara <araismot234@gmail.com>
2025-08-22 21:06:22 +0600

    Enter two integers

---
+++ Add two numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      int number1, number2, sum;
+
+      cout << "Enter two integers: ";
+      cin >> number1 >> number2 ;
+
+      sum = number1 + number2;
+
+      cout <<  number1 << " + " << number2 << "=" << sum;
+      return 0;
+}
