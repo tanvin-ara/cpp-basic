02f0db33754d9451a5ff7be04645ff4f8c8bfe7f
tanvin-ara <araismot234@gmail.com>
2025-08-22 22:05:51 +0600

    multiply two floating-numbers

---
+++ multiply two floating-point numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     float num1, num2, product;
+
+     cout << "Enter two numbers:";
+     cin >> num1 >> num2;
+     product = num1 * num2;
+     cout << "product = " << product << endl;
+     return 0;
+               }
