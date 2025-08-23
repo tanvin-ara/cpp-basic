d0712c3d2312a695a42d29aaeeafdf5df0e96026
tanvin-ara <araismot234@gmail.com>
2025-08-24 00:08:56 +0600

    area and perimeter of rectangle

---
+++ Area and perimeter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float l, w;
+ cout << "Enter length and width: ";
+ cin >> l >> w;
+ cout << "Area: " << l * w << endl;
+ cout << "Perimeter: " << 2 * (l + w) << endl;
+return 0;
+
+}
