d390123203aed482d3a5dfd71e6dd371f9493de5
tanvin-ara <araismot234@gmail.com>
2025-08-24 02:34:31 +0600

    print alphabets

---
+++ print alphabets.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+char ch;
+cout << "alphabet from A to Z: " << endl;
+for (ch = 'A'; ch <= 'z'; ch++){
+          cout << ch << " ";
+}
+return 0;
+}
