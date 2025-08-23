78731a38d338090cd500ea2bf9c7d3d1071e052b
tanvin-ara <araismot234@gmail.com>
2025-08-24 01:52:43 +0600

    vowel or consonant character

---
+++ vowel or consonant character.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+   char a;
+     cout << "Enter an character: ";
+     cin >> a;
+     if (a=='a'|| a=='e'|| a=='i'||a=='o'||a=='u'||
+          a=='A'|| a=='E'|| a=='I'||a=='O'||a=='U')
+    {
+      cout << a << "is a vowel.";
+
+    } else{
+              cout << a << "is a consonant.";
+
+    }
+
+
+
+     return 0;
+
+
+     }
+
