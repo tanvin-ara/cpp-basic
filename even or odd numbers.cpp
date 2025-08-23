38bb3b08cc51c0ff210d71cbda7fd53f6d265f5d
tanvin-ara <araismot234@gmail.com>
2025-08-24 01:33:51 +0600

    even or odd numbers

---
+++ even or odd numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int a;
+
+     cout << "Enter an integer: ";
+     cin >> a;
+     (a % 2 == 0) ?  cout << a << " is even." : cout << a << " is odd.";
+     return 0;
+
+
+     }
