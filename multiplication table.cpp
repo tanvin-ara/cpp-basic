cce9a2944f36070a57fdbbfd3a4ecb9d64c9053d
tanvin-ara <araismot234@gmail.com>
2025-08-24 13:05:28 +0600

    multiplication table

---
+++ multiplication table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int a;
+     cout << "positive number: ";
+     cin >> a;
+          for(int i = 1; i <= 10; ++i){
+                    cout << a << " * " << i << " = " << a * i << endl;
+          }
+return 0;
+}
+
