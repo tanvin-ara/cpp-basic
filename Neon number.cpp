6944bd0b3cc7d2e939c499f58948df14e3df3549
tanvin-ara <araismot234@gmail.com>
2025-11-26 10:05:15 +0600

    neon number

---
+++ Neon number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int square = num * num;
+    int sum = 0;
+
+    while (square != 0) {
+        int digit = square % 10;
+        sum += digit;
+        square /= 10;
+    }
+
+    if (sum == num)
+        cout << num << " is a Neon Number." << endl;
+    else
+        cout << num << " is NOT a Neon Number." << endl;
+
+    return 0;
+}
