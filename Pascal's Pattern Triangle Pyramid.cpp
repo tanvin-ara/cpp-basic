cadc01a7a7c91d53e3193c86c8f610ecfd0945ce
tanvin-ara <araismot234@gmail.com>
2025-11-27 10:07:00 +0600

    Pascal's Pattern Triangle Pyramid

---
+++ Pascal's Pattern Triangle Pyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[50][50];
+
+    for (int i = 0; i < n; i++) {
+        a[i][0] = a[i][i] = 1;
+        for (int j = 1; j < i; j++)
+            a[i][j] = a[i-1][j-1] + a[i-1][j];
+
+        // print row
+        for (int j = 0; j <= i; j++)
+            cout << a[i][j] << " ";
+
+        cout << endl;
+    }
+}
