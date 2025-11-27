004a50543df7a8ce01cbd2cc14d615e611c4d40f
tanvin-ara <araismot234@gmail.com>
2025-11-27 22:43:26 +0600

    Find Quotient and Remainder

---
+++ Find Quotient and Remainder.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dividend, divisor;
+    cout << "Enter dividend and divisor: ";
+    cin >> dividend >> divisor;
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+    return 0;
+}
