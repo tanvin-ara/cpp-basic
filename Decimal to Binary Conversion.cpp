fdbd8f8487102899a1e21603ad0ecc958e4cee2b
tanvin-ara <araismot234@gmail.com>
2025-11-27 20:59:51 +0600

    Octal to Decimal Conversion

--- Decimal to Binary Conversion.cpp
+++ Decimal to Binary Conversion.cpp
@@ -7,8 +7,7 @@ int main() {
     cout << "Enter a decimal number: ";
     cin >> num;
 
-    string binary = bitset<32>(num).to_string(); // 32-bit binary
-    // Remove leading zeros
+    string binary = bitset<32>(num).to_string();
     binary = binary.substr(binary.find('1'));
     cout << "Binary: " << binary << endl;
 
