9255cc95a319ed838ad3c52591f32f58edb3cac8
tanvin-ara <araismot234@gmail.com>
2025-11-27 10:02:45 +0600

    Full Diamond Shape Pyramid

---
+++ Full Diamond Shape Pyramid.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+
+        for (int s = 1; s <= n - i; s++)
+            cout << " ";
+
+        for (int star = 1; star <= 2 * i - 1; star++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+
+        for (int s = 1; s <= n - i; s++)
+            cout << " ";
+
+        for (int star = 1; star <= 2 * i - 1; star++)
+            cout << "*";
+
+        cout << endl;
+    }
+}
