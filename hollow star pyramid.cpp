359df698209a8eed87cfd8c7cf4cad66a15000d0
tanvin-ara <araismot234@gmail.com>
2025-11-27 09:39:42 +0600

    hollow star pyramid

---
+++ hollow star pyramid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+
+        for (int s = 0; s < n - i; s++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            if (i == n || j == 1 || j == (2 * i - 1))
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
